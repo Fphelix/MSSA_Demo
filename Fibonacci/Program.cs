1cbc71cf38315b2d25a16e3fbad83bdf338772cb
Fphelix <77512995+Fphelix@users.noreply.github.com>
2021-03-06 00:18:21 -0500

    Fibbonacci

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i1 = 0;
+            int i2 = 1;
+
+        for (int i = 0; i < 20; i++)
+            {
+                int i3 = i1 + i2;
+                Console.WriteLine(i1);
+                i1 = i2;
+                i2 = i3;
+          
+            }
+           
+
+        }
+    }     
+}
